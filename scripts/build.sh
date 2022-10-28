d782ecc6f53d94159b63a67d1f78a50072cabe4a
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-10-28 11:27:26 +0200

    Add files via upload

---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+g++ -std=gnu++20 -Wall -Wextra -g -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -O2 -o $1.exe $1.cpp
---
+++ scripts/primary_monitor.sh
@@ -0,0 +1,2 @@
+xrandr --output eDP --auto
+xrandr --output HDMI-A-0 --off
